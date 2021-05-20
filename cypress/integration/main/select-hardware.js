7faac4d0b5639e27f1d9737c3d0cefb21c73cf76
siobahnschuck <sschuc@saic.edu>
2021-05-20 10:21:13 -0500

    Select Hardware Complete Test Action 11

--- cypress/integration/main/select-hardware.js
+++ cypress/integration/main/select-hardware.js
@@ -1,32 +1,60 @@
+//GLOBAL VARIABLES 
+
+//select hardware
+const SHOP_LINK = 'https://eloquent-lich-king.netlify.app/homepass/'
+const SUPERPOD_CON = '.IjAmP'
+const WIFI6_PRICE = '.hrMwYB'
+const COLOR = '.jqbxUv'
+const NEXT = '.ProductSelectorFooter__NextButton-saqfh4-4'
+const HEADER = '.Header__HeaderWrapper-q8vck-1'
+const MODAL_CLOSE = '.eaYebE:first'
+
 
 // MAIN NAVIGATION 
 describe('HOMEPASS SHOP', () => {
-  it('successfully loads', () => {
-    cy.visit('https://eloquent-lich-king.netlify.app/homepass/')
+  it('TEST ELEMENTS ON PAGE', () => {
+    cy.visit(SHOP_LINK)
     cy.contains('Select your Hardware')
     //NAV BAR 
-    cy.get('.Header__HeaderInnerWrapper-q8vck-2').contains('HomePass')
+    //verify homepass logo
+    cy.get('img[alt="HomePass"]')
+    cy.get(HEADER).contains("What's Included")
+    cy.get(HEADER).contains('Smart Home Experience')
+    cy.contains("Compare Products").click()
+    cy.get(MODAL_CLOSE).click({force:true})
   })
 })
-
-
-
-
 
 //SELECT HARDWARE 
 
 describe('SELECT YOUR HARDWARE - SUPERPOD SILVER', () => {
   it('TEST SCENERIO 1.X', () => {
-    cy.contains('Silver').click()
-    cy.get('.IjAmP').contains('$99 each')
-    cy.contains('Next').click()
+    cy.get(COLOR).contains('Silver').click()
+    cy.get(SUPERPOD_CON).contains('$99 each')
+    cy.get(NEXT).click()
   })
 })
 
 describe('SELECT YOUR HARDWARE - SUPERPOD CHAMPAGNE', () => {
   it('TEST SCENERIO 1.X', () => {
-    cy.contains('Champagne').click()
-    cy.get('.IjAmP').contains('$99 each')
-    cy.contains('Next').click()
+    cy.get(COLOR).contains('Champagne').click()
+    cy.get(SUPERPOD_CON).contains('$99 each')
+    cy.get(NEXT).click()
+  })
+})
+
+describe('SELECT YOUR HARDWARE - SUPERPOD WITH WIFI 6 SILVER', () => {
+  it('TEST SCENERIO 1.X', () => {
+    cy.get(WIFI6_PRICE).eq(1).contains('$159 each')
+    cy.get(COLOR).eq(1).contains('Silver').click()
+    cy.get(NEXT).click()
+  })
+})
+
+describe('SELECT YOUR HARDWARE - SUPERPOD WITH WIFI 6 CHAMPAGNE', () => {
+  it('TEST SCENERIO 1.X', () => {
+    cy.get(WIFI6_PRICE).eq(1).contains('$159 each')
+    cy.get(COLOR).eq(1).contains('Champagne').click()
+    cy.get(NEXT).click()
   })
 })
