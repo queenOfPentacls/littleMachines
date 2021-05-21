9d08967841aaa4f3a7f7efc053c363e03fae4287
siobahnschuck <sschuc@saic.edu>
2021-05-21 10:52:54 -0500

    add membership tests

--- cypress/integration/main/select-hardware.js
+++ cypress/integration/main/select-hardware.js
@@ -1,28 +1,30 @@
 //GLOBAL VARIABLES 
 
 //select hardware
-const SHOP_LINK = 'https://eloquent-lich-king.netlify.app/homepass/'
-const SUPERPOD_CON = '.IjAmP'
-const WIFI6_PRICE = '.hrMwYB'
-const COLOR = '.jqbxUv'
+const DEPLOYED_LINK = 'https://eloquent-lich-king.netlify.app/homepass/'
+const LOCAL_HOST = 'http://localhost:3000/homepass'
+const SUPERPOD_CON = '.eOLOVD'
+const PRICE = '.dcxOIm'
+const COLOR_WEB = '.jqbxUv'
+const COLOR_LOCAL = '.eRitdj'
 const NEXT = '.ProductSelectorFooter__NextButton-saqfh4-4'
 const HEADER = '.Header__HeaderWrapper-q8vck-1'
-const MODAL_CLOSE = '.eaYebE:first'
+const MODAL_CLOSE_WEB = '.eaYebE:first'
+const MODAL_CLOSE_LOCAL = '.Modal__ModalToggleButton-sc-1ywrvd4-3'
 
 
 // MAIN NAVIGATION 
 describe('HOMEPASS SHOP', () => {
   it('TEST ELEMENTS ON PAGE', () => {
-    cy.visit(SHOP_LINK)
-    cy.wait(1000)
+    cy.visit(LOCAL_HOST)
     cy.contains('Select your Hardware')
     //NAV BAR 
     //verify homepass logo
     cy.get('img[alt="HomePass"]')
     cy.get(HEADER).contains("What's Included")
     cy.get(HEADER).contains('Smart Home Experience')
-    cy.contains("Compare Products").click()
-    cy.get(MODAL_CLOSE).click({force:true})
+    cy.contains("Compare Products")
+    // cy.get(MODAL_CLOSE_LOCAL).click()
   })
 })
 
@@ -30,32 +32,32 @@ describe('HOMEPASS SHOP', () => {
 
 describe('SELECT YOUR HARDWARE - SUPERPOD SILVER', () => {
   it('TEST SCENERIO 1.X', () => {
-    cy.get(COLOR).contains('Silver').click()
-    cy.get(SUPERPOD_CON).contains('$99 each')
+    cy.get(COLOR_LOCAL).contains('Silver').click()
+    cy.get(PRICE).contains('$99 each')
     cy.get(NEXT).click()
   })
 })
 
 describe('SELECT YOUR HARDWARE - SUPERPOD CHAMPAGNE', () => {
   it('TEST SCENERIO 1.X', () => {
-    cy.get(COLOR).contains('Champagne').click()
-    cy.get(SUPERPOD_CON).contains('$99 each')
+    cy.get(COLOR_LOCAL).contains('Champagne').click()
+    cy.get(PRICE).contains('$99 each')
     cy.get(NEXT).click()
   })
 })
 
 describe('SELECT YOUR HARDWARE - SUPERPOD WITH WIFI 6 SILVER', () => {
   it('TEST SCENERIO 1.X', () => {
-    cy.get(WIFI6_PRICE).eq(1).contains('$159 each')
-    cy.get(COLOR).eq(1).contains('Silver').click()
+    cy.get(PRICE).eq(1).contains('$159 each')
+    cy.get(COLOR_LOCAL).eq(1).contains('Silver').click()
     cy.get(NEXT).click()
   })
 })
 
 describe('SELECT YOUR HARDWARE - SUPERPOD WITH WIFI 6 CHAMPAGNE', () => {
   it('TEST SCENERIO 1.X', () => {
-    cy.get(WIFI6_PRICE).eq(1).contains('$159 each')
-    cy.get(COLOR).eq(1).contains('Champagne').click()
+    cy.get(PRICE).eq(1).contains('$159 each')
+    cy.get(COLOR_LOCAL).eq(1).contains('Champagne').click()
     cy.get(NEXT).click()
   })
 })
---
+++ cypress/integration/main/select-membership.js
@@ -0,0 +1,55 @@
+
+const LOCAL_HOST_3 = 'http://localhost:3000/homepass/customize-membership'
+const HEADER = '.Header__HeaderWrapper-q8vck-1'
+const CART_BTN = '.ProductSelectorFooter__NextButton-saqfh4-4'
+const BACK_BTN = '.ProductSelectorFooter__BackButton-saqfh4-5'
+const MEMBERSHIP = '.MembershipComponent__MembershipButton-a1q3q4-2'
+const PRICE = '.MembershipComponent__PriceLine-a1q3q4-11'
+const MODAL_CLOSE_LOCAL = '.Modal__ModalToggleButton-sc-1ywrvd4-3'
+
+
+
+
+describe('HOMEPASS SELECT MEMBERSHIP', () => {
+  it('TEST ELEMENTS ON PAGE', () => {
+    cy.visit(LOCAL_HOST_3)
+    cy.get('img[alt="HomePass"]')
+    cy.get(HEADER).contains("What's Included")
+    cy.get(HEADER).contains('Smart Home Experience')
+    cy.contains("Membership Benefits").click()
+    cy.get(MODAL_CLOSE_LOCAL).eq(1).click()
+    //information modal
+    cy.get('.bZcYBD').click()
+    cy.get('.lbFxBo').click()
+  })
+})
+
+describe('CART DEACTIVATED', () => {
+  it('DEAVTIVATED', () => {
+    cy.get(CART_BTN).should('be.disabled')
+  })
+})
+
+describe('1 YEAR', () => {
+  it('TEST MEMBERSHIP', () => {
+    cy.get(MEMBERSHIP).contains('1 year').click()
+    cy.get(PRICE).eq(0).contains('$99')
+    cy.get(CART_BTN).should('not.be.disabled').click()
+  })
+})
+
+describe('2 YEAR', () => {
+  it('TEST MEMBERSHIP', () => {
+    cy.get(MEMBERSHIP).contains('2 year').click()
+    cy.get(PRICE).eq(1).contains('$198')
+    cy.get(CART_BTN).should('not.be.disabled').click()
+  })
+})
+
+describe('3 YEAR', () => {
+  it('TEST MEMBERSHIP', () => {
+    cy.get(MEMBERSHIP).contains('3 year').click()
+    cy.get(PRICE).eq(2).contains('$297')
+    cy.get(CART_BTN).should('not.be.disabled').click()
+  })
+})
