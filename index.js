97bd3a0278fac7b788e6ea662a984cac6ecb0ddc
siobahnschuck <sschuc@saic.edu>
2021-05-27 11:48:16 -0500

    create custom output

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const cypress_chrome = core.getInput('cypress_chrome')
+  console.log(cypress_chrome)
+  // const run = core.getInput('run')
+  // console.log(run)
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
