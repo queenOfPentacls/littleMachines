0f9b7f8c9534b4d4c3f8461068d0d5ef3e91b190
siobahnschuck <sschuc@saic.edu>
2021-06-11 14:10:34 -0500

    testing update index

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ try {
   console.log(`cypress test ${cypress_chrome}`)
   // const payload = JSON.stringify(github.context, undefined, 2)
   // console.log(`The event payload abcd: ${payload}`);
+  console.log('this is a test')
 } catch (error) {
   core.setFailed(error.message);
 }
